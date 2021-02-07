901d694e8ba700bc8b712665c687172a026bdc20
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-07 19:18:48 +0700

    feat: add content with avatar assets

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,11 +12,37 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Runyasak")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
-                .foregroundColor(.white)
-                .padding()
+            VStack {
+                Image("avatar")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Runyasak")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 25))
+
+                Divider()
+                
+
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .overlay(
+                        HStack {
+                            Image(systemName: "phone.fill").foregroundColor(.green)
+                            Text("runyasak.c@gmail.com")
+                        }
+                    )
+                    .padding()
+            }
         }
     }
 }
