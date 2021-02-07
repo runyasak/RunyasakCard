457b6d5345b997b6cebcfd09301c8e9d5e69ab34
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-07 18:00:50 +0700

    Initial Commit

---
+++ Shared/RunyasakCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunyasakCardApp.swift
+//  Shared
+//
+//  Created by Runyasak Chaengnaimuang on 7/2/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct RunyasakCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
